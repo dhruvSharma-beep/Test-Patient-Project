c39cf5ea89c019ce60f9f6a96f960eda9f519b07
dhruvSharma-beep <70944915+dhruvSharma-beep@users.noreply.github.com>
2020-11-27 12:56:18 +0530

    Update DeletePatientEmulator.php

--- DeletePatientEmulator.php
+++ DeletePatientEmulator.php
@@ -1,14 +1,14 @@
 <?php require_once 'PatientFunction.php';
 
-$O_Delete_Patient = new DeletePatient;
-$Patient_Delete_Data = (file_get_contents("php://input"));
-$J_Patient_Delete_Data = json_decode($Patient_Delete_Data, true);
-$Patient_Delete_Id = $J_Patient_Delete_Data['I_Patient_Delete_Id'];
+$oDeletePatient = new DeletePatient;
+$PatientDelete = (file_get_contents("php://input"));
+$jPatientDelete = json_decode($PatientDelete, true);
+$iDeleteId = $jPatientDelete['I_Patient_Delete_Id'];
 
 
-if (!empty($Patient_Delete_Id)) {
-    $Q_Sql_Delete = $O_Delete_Patient->DeletePatientData($Patient_Delete_Id);
-    if ($Q_Sql_Delete == true) {
+if (!empty($iDeleteId)) {
+    $qSqlDelete = $oDeletePatient->DeletePatientData($iDeleteId);
+    if ($qSqlDelete == true) {
         echo "data deleted successfully";
     } else {
         echo "unable to delete data";
