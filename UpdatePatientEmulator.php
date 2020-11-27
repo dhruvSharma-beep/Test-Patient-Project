b17874de2b6f8f543b2e6adf1ae1e7c8cafa5ddf
dhruvSharma-beep <70944915+dhruvSharma-beep@users.noreply.github.com>
2020-11-27 14:28:46 +0530

    Update UpdatePatientEmulator.php

--- UpdatePatientEmulator.php
+++ UpdatePatientEmulator.php
@@ -1,21 +1,21 @@
 <?php
 require_once 'PatientFunction.php';
 
-$O_Update_Patient = new UpdatePatientDetails();
-$Patient_Update_Details = (file_get_contents("php://input"));
-$Update_patient_Details = json_decode($Patient_Update_Details, true);
-$I_Patient_id = $Update_patient_Details['I_Patient_id'];
-$S_First_Name = $Update_patient_Details['S_First_Name'];
-$S_Last_Name = $Update_patient_Details['S_Last_Name'];
-$S_Email_Id = $Update_patient_Details['S_Email_Id'];
-$I_Contact_No = $Update_patient_Details['I_Contact_No'];
-$S_Address = $Update_patient_Details['S_Address'];
-$D_Dob = $Update_patient_Details['D_Dob'];
-$B_Gender = $Update_patient_Details['B_Gender'];
+$oUpdatePatient = new UpdatePatientDetails();
+$PatientUpdateDetails = (file_get_contents("php://input"));
+$jUpdatePatientDetails = json_decode($PatientUpdateDetails, true);
+$iPatientId = $jUpdatePatientDetails['iPatient_id'];
+$sFirstName = $jUpdatePatientDetails['First_Name'];
+$sLastName = $jUpdatePatientDetails['Last_Name'];
+$sEmailId = $jUpdatePatientDetails['Email'];
+$iContactNo = $jUpdatePatientDetails['MobileNumber'];
+$sAddress = $jUpdatePatientDetails['Address'];
+$dDob = $jUpdatePatientDetails['Dob'];
+$bGender = $jUpdatePatientDetails['Gender'];
 
-if (!empty($I_Patient_id)) {
-    $Q_Patient_Sql = $O_Update_Patient->PatientUpdate($S_First_Name, $S_Last_Name, $S_Email_Id, $I_Contact_No, $S_Address, $D_Dob, $B_Gender, $I_Patient_id);
-    if ($Q_Patient_Sql) {
+if (!empty($iPatientId)) {
+    $qPatientSql = $oUpdatePatient->PatientUpdate($sFirstName, $sLastName, $sEmailId, $iContactNo, $sAddress, $dDob, $bGender, $iPatientId);
+    if ($qPatientSql) {
 
         echo "data is updated successfully";
     } else {
