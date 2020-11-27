56201bada170480d6a4420db5d291a05fa12a4f8
dhruvSharma-beep <70944915+dhruvSharma-beep@users.noreply.github.com>
2020-11-27 12:51:55 +0530

    Update ShowPatientEmulator.php

--- ShowPatientEmulator.php
+++ ShowPatientEmulator.php
@@ -1,12 +1,12 @@
 <?php require_once 'PatientFunction.php';
 
-$Fetch_Patient_Detail = new PatientDetails();
-$O_Patient_Sql = $Fetch_Patient_Detail->FetchPatientDetails();
+$oPatientDetail = new PatientDetails();
+$qPatientSql = $oPatientDetail->FetchPatientDetails();
 
 
 
-while ($row = mysqli_fetch_array($O_Patient_Sql)) {
-    $Patient_data[] = $row;
+while ($row = mysqli_fetch_array($qPatientSql)) {
+    $Patientdata[] = $row;
 }
 
-echo json_encode($Patient_data);
+echo json_encode($Patientdata);
